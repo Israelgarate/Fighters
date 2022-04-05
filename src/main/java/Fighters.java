1da7a5a0fcb33419d811f7974922e6e22b1cfd06
Israel Garate <israelgarate@gmail.com>
2022-04-04 21:05:23 -0400

    Initial commit

---
+++ src/main/java/Fighters.java
@@ -0,0 +1,26 @@
+public class Fighters {
+
+    public  String declareWinner(Fighter fighter1, Fighter fighter2, String firstAttacker) {
+        String winner = "";
+        String turn = firstAttacker;
+        while ((fighter1.health > 0 || fighter2.health > 0) && winner == "") {
+
+            if (turn.equals(fighter1.name)){
+                fighter2.health -= fighter1.damagePerAttack;
+                turn = fighter2.name;
+                if(fighter2.health <= 0){
+                    winner = fighter1.name;
+                }
+            }
+            else if (turn.equals(fighter2.name)){
+                fighter1.health -= fighter2.damagePerAttack;
+                turn = fighter1.name;
+                if(fighter1.health <= 0){
+                    winner = fighter2.name;
+                }
+            }
+        }
+        return winner;
+    }
+
+}
